ac9d8e99c4df8711a887013a898155cc29b23fc1
tongyipush <tongyipush777888@126.com>
2021-01-15 18:25:51 +0800

    增加剑法

---
+++ src/main/java/com/atguigu/huashan/Duguqiujian.java
@@ -0,0 +1,10 @@
+package com.atguigu.huashan;
+
+public class Duguqiujian {
+    public static void main(String[] args) {
+        System.out.println("独孤求🗡");
+        System.out.println("独孤求🗡");
+        System.out.println("独孤求🗡");
+        System.out.println("独孤求🗡");
+    }
+}
