9d56d06288e2dec63c8741d471e02c51c05dda0c
tongyipush <tongyipush777888@126.com>
2021-01-16 10:47:07 +0800

    独孤9🗡

---
+++ src/main/java/com/atguigu/huashan/Dugu9jian.java
@@ -0,0 +1,11 @@
+package com.atguigu.huashan;
+
+public class Dugu9jian {
+    public static void main(String[] args) {
+        System.out.println("独孤9🗡");
+        System.out.println("独孤9🗡");
+        System.out.println("独孤9🗡");
+        System.out.println("独孤9🗡");
+        System.out.println("独孤9🗡");
+    }
+}
